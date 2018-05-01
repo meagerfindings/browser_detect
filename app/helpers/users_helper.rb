37e7c62e496c459b4a34b6003d938c199284ebc1
Mat Greten <mgreten@gmail.com>
2018-05-01 12:56:14 -0600

    added visits to user profile page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+  @visitor_visits = {}
+
+  def find_user_visits(user_email)
+    @visitor_visits = {}
+
+    visitors = Visitor.all
+    visitors.each do |visitor|
+      @visitor_visits[visitor.id]
+      if visitor.email == user_email
+        @visitor_visits[visitor.id] = visitor
+      end
+    end
+    @visitor_visits
+  end
 end
