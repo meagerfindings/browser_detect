d89499fcd727bb3ef60722835042a6e59754dca3
Mat Greten <mgreten@gmail.com>
2018-05-02 11:56:11 -0600

    updated user_auth to account for additional validations added during password reset implementation

--- spec/support/user_auth.rb
+++ spec/support/user_auth.rb
@@ -1,21 +1,31 @@
+# frozen_string_literal: true
+
 module UserAuth
   def employee_login
     email = "#{rand(2_000)}_EMPLOYEE_#{ENV['EMPLOYEE_EMAIL_DOMAIN']}"
+    password = "bumble bee tuna#{rand(50_000)}"
     employee = User.create(email: email,
-                           password: "bumble bee tuna#{rand(50_000)}",
+                           password: password,
+                           password_confirmation: password,
                            employee: true,
-                           email_confirmed: true)
-    employee.save
+                           email_confirmed: true,
+                           first_name: 'employee',
+                           last_name: 'extraordinaire')
+    employee.save(validation: false)
     session[:user_id] = employee.id
   end
 
   def user_login
     email = "#{rand(2_000)}_NON-EMPLOYEE_@browser_detect.com"
-    employee = User.create(email: email,
-                           password: "bumble bee tuna#{rand(50_000)}",
-                           employee: false,
-                           email_confirmed: true)
-    employee.save
-    session[:user_id] = employee.id
+    password = "do you have a reservation?#{rand(50_000)}"
+    user = User.create(email: email,
+                       password: password,
+                       password_confirmation: password,
+                       employee: false,
+                       email_confirmed: true,
+                       first_name: 'Guybrush',
+                       last_name: 'threepwood')
+    user.save
+    session[:user_id] = user.id
   end
 end
