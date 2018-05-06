74a377d56a27871c1c4d3a128d8abd70375b4a63
Mat Greten <mgreten@gmail.com>
2018-05-06 14:33:34 -0600

    mailer specs

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
   describe 'password_reset' do
-    let(:user) { User.last}
+    let(:user) { User.last }
     let(:mail) { UserMailer.password_reset(user) }
 
     before(:each) do
@@ -25,11 +25,38 @@ RSpec.describe UserMailer, type: :mailer do
     it 'renders the headers' do
       expect(mail.subject).to eq('Password Reset')
       expect(mail.to).to eq(['to@example.org'])
-      expect(mail.from).to eq([ENV['EMAIL_TARGET']])
+      expect(mail.from).to eq([ENV['EMAIL_USERNAME']])
     end
 
     it 'renders the body' do
       expect(mail.body.encoded).to match("To reset your password click the URL below.\r\n\r\nhttp://localhost:3000/password_resets/token/edit\r\n\r\nIf you did not request your password to be reset please ignore this email and your password will stay as it is.")
+    end
+  end
+
+  describe 'password_reset' do
+    let(:user) { User.last }
+    let(:mail) { UserMailer.registration_confirmation(user) }
+
+    before(:each) do
+      temp_user = User.new(email:                 'to@example.org',
+                           password:              'password',
+                           password_confirmation: 'password',
+                           employee:              false,
+                           email_confirmed:       false,
+                           first_name:            'Guybrush',
+                           last_name:             'Threepwood',
+                           confirm_token:  "token#{rand(200)}")
+      temp_user.save
+    end
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq('BrowserDetect Registration Confirmation')
+      expect(mail.to).to eq(["#{user.email}"])
+      expect(mail.from).to eq([ENV['EMAIL_USERNAME']])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match("Hello #{user.first_name},\r\n\r\nThank you for registering your account with BrowserDetect. To confirm your registration, please click this URL: http://localhost:3000/users/#{user.confirm_token}/confirm_email\r\n\r\nHave a wonderful day!\r\n\r\nWith best regards,\r\nThe BrowserDetect Team")
     end
   end
 end
--- spec/mailers/visitor_mailer_spec.rb
+++ spec/mailers/visitor_mailer_spec.rb
@@ -1,5 +1,21 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe VisitorMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:visitor) { Visitor.last }
+  let(:mail) { VisitorMailer.with(visitor: visitor).share_information_email }
+
+  it 'renders the headers' do
+    temp_vis = Visitor.new(email: 'to@example.org',
+                           browser: 'FireFox',
+                           version: '59',
+                           comp_mode: false,
+                           os: 'Chrome OS')
+    temp_vis.save
+
+    expect(mail.subject).to eq("BrowserDetect: #{visitor.email}")
+    expect(mail.to).to eq([ENV['EMAIL_TARGET']])
+    expect(mail.from).to eq([ENV['EMAIL_USERNAME']])
+  end
 end
